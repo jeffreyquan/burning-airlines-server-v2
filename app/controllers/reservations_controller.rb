def9f05399f772f6da6ae7d5d264a6c554adff3b
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-11-21 00:22:39 +1100

    changed reservation params to accept row and column

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -69,6 +69,6 @@ class ReservationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def reservation_params
-      params.require(:reservation).permit(:row_no, :col_no, :flight_id, :user_id)
+      params.require(:reservation).permit(:row, :column, :flight_id, :user_id)
     end
 end
